dfde5e9f8c95c4bd266db298c45d2c8a2fd9c980
juba2320 <juba2320@colorado.edu>
2023-09-12 01:33:36 +0000

    added get_column

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,15 @@
+from array import array
+
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    
+    file = open(file_name, 'r')
+    col_vals = []
+    
+    for line in file:
+        l = line.split(',')
+        if l[query_column] == l[query_value]:
+            col_vals = col_vals.append(l[result_column])
+        
+    file.close()
+    
+    return col_vals
