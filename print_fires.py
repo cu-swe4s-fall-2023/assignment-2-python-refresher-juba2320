5b4df7bdbba651c2bef90f962b000b7712760591
juba2320 <juba2320@colorado.edu>
2023-09-20 01:59:57 +0000

    update my_utils, print_fires, and run.sh

--- print_fires.py
+++ print_fires.py
@@ -1,8 +1,28 @@
 import my_utils as utils
+import argparse 
 
-country='United States of America'
-county_column = 0
-fires_column = 3
-file_name = 'Agrofood_co2_emission.csv'
-fires = utils.get_column(file_name = file_name, query_column = county_column , query_value = country, result_column = fires_column)
+parser = argparse.ArgumentParser(
+            description = "Take in command line arguments for country, country_column, fires_column, file_name",
+            prog = "HW#3")
+
+parser.add_argument('--file_name',
+                   type = str,
+                   help = 'Name of file',
+                   required = True)
+parser.add_argument('--country',
+                   type = str,
+                   help = 'Country name to look up',
+                   required = True)
+parser.add_argument('--country_column',
+                   type = int, 
+                   help = 'Country column number from index 0',
+                   required = True)
+parser.add_argument('--fires_column',
+                   type = int,
+                   help = 'Fires column number from index 0',
+                   required = False)
+args = parser.parse_args()
+
+fires = utils.get_column(file_name = args.file_name, query_column = args.country_column, 
+                         query_value = args.country, result_column = args.fires_column)
 print(fires)
--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-set -e
 set -u
-set -o
+set -o 
+
+a="Agrofood_co2_emission.csv"
+b="Afghanistan"
+c=0 
+d=3
 
 echo "Running print_fires.py"
+python print_fires.py --file_name $a --country $b --country_column $c --fires_column $d
+
+echo "Running print_fires.py again with error" 
 python print_fires.py
+
+echo "Running print_fires.py again with error"
+python print_fires.py --file_name $a --country $c --country_column $b --fires_column $d
